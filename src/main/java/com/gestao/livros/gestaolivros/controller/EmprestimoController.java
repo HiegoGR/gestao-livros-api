1bf2c7336bc61e9787fae5dc1b9d9245f203fa92
HiegoGeorge <hiego.oliveira@sga.pucminas.br>
2024-08-24 10:36:28 -0300

    CRUD de emprestimo

--- src/main/java/com/gestao/livros/gestaolivros/controller/EmprestimoController.java
+++ src/main/java/com/gestao/livros/gestaolivros/controller/EmprestimoController.java
@@ -1,11 +1,53 @@
 package com.gestao.livros.gestaolivros.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gestao.livros.gestaolivros.dto.EmprestimoDto;
+import com.gestao.livros.gestaolivros.service.EmprestimoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("${url.base.controller}")
 public class EmprestimoController {
+
+    @Autowired
+    private EmprestimoService emprestimoService;
+
+    @GetMapping("/loans")
+    public ResponseEntity<List<EmprestimoDto>> getAll(){
+        List<EmprestimoDto> list = emprestimoService.getAllEmprestimo();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/loans/{id}")
+    public ResponseEntity<Optional<EmprestimoDto>> findByIdLoan(@PathVariable Long id){
+        Optional<EmprestimoDto> dto = emprestimoService.findEmprestimoById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping("/loans")
+    public ResponseEntity<EmprestimoDto> insertLoan(@RequestBody EmprestimoDto dto){
+        EmprestimoDto newDTO = emprestimoService.save(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDTO);
+    }
+
+    @PutMapping("/loans")
+    public ResponseEntity<EmprestimoDto> updateLoan(@RequestBody EmprestimoDto dto){
+        EmprestimoDto newDto = emprestimoService.update(dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @DeleteMapping("/loans/{id}")
+    public ResponseEntity<Void> deleteLoan(@PathVariable Long id){
+        emprestimoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/gestao/livros/gestaolivros/service/EmprestimoService.java
+++ src/main/java/com/gestao/livros/gestaolivros/service/EmprestimoService.java
@@ -1,9 +1,73 @@
 package com.gestao.livros.gestaolivros.service;
 
+import com.gestao.livros.gestaolivros.dto.EmprestimoDto;
+import com.gestao.livros.gestaolivros.entities.EmprestimoEntity;
+import com.gestao.livros.gestaolivros.mapper.EmprestimoMapper;
+import com.gestao.livros.gestaolivros.repository.EmprestimoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
+@Transactional
 public class EmprestimoService {
 
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
+    @Autowired
+    private EmprestimoMapper emprestimoMapper;
+
+
+    public List<EmprestimoDto> getAllEmprestimo() {
+        return emprestimoRepository.findAll().stream()
+                .map(emprestimoMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    //método para buscar Empréstimos pelo id
+    public Optional<EmprestimoDto> findEmprestimoById(Long id) {
+        return emprestimoRepository.findById(id).map(emprestimoMapper::toDto);
+    }
+
+    //método para salvar Empréstimos
+    public EmprestimoDto save(EmprestimoDto emprestimoDto) {
+        EmprestimoEntity emprestimo = emprestimoMapper.toEntity(emprestimoDto);
+        emprestimo = emprestimoRepository.save(emprestimo);
+        return emprestimoMapper.toDto(emprestimo);
+    }
+
+    //método para atualizar  Empréstimos na base
+    public EmprestimoDto update(EmprestimoDto emprestimoDto) {
+
+        if (emprestimoDto.getId() == null) {
+            throw new RuntimeException("Informe id do empréstimo para que o mesmo possa ser alterado");
+        }
+        // Verifica se o Empréstimos existe na base
+        if (!emprestimoRepository.existsById(emprestimoDto.getId())) {
+            throw new RuntimeException("Empréstimo não encontrado com o ID: " + emprestimoDto.getId());
+        }
+
+        EmprestimoEntity emprestimo = emprestimoMapper.toEntity(emprestimoDto);
+        emprestimo.setId(emprestimoDto.getId());
+
+        // atualiza Empréstimo na base
+        emprestimo = emprestimoRepository.save(emprestimo);
+
+        return emprestimoMapper.toDto(emprestimo);
+    }
+
+    //método para excluir um Empréstimo
+    public void delete(Long id) {
+        // Verifica se o Empréstimo existe na base para ser excluido
+        if (!emprestimoRepository.existsById(id)) {
+            throw new RuntimeException("Empréstimo não encontrado com o ID: " + id);
+        }
+        emprestimoRepository.deleteById(id);
+    }
 }
